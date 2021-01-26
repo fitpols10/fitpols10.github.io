a7e7001d4df7415556f9008ab4a31409814fd99c
fitpols10 <50966590+fitpols10@users.noreply.github.com>
2021-01-26 13:51:54 -0300

    Add files via upload

---
+++ change.js
@@ -0,0 +1,15 @@
+<DOCTYPE html>
+<html lang=''pt-br>
+<head>
+<Title>The Don Kiluminati>/Title>
+<meta charset''utf'8''>
+</head>
+<body>
+fitpols10.github.io
+</body>
+<head>
+<style type='text/css">
+<p style="color: blue".</p>
+ <script type="text/javascript">
+    alert('Hello World!');
+</html>
